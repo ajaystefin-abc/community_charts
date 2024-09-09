4b6a22232b832bb6c1eff0a78411fb3bd15080d0
Ajay <ajaystefin.s-v@adityabirlacapital.com>
2024-09-09 18:45:03 +0530

    border check

--- community_charts_flutter/lib/src/canvas/pie_painter.dart
+++ community_charts_flutter/lib/src/canvas/pie_painter.dart
@@ -28,7 +28,7 @@ class PiePainter {
     final innerRadius = canvasPie.innerRadius;
 
     for (var slice in canvasPie.slices) {
-      print('AJ--');
+      print('AJ--1');
       if (slice.isBorderRequired) {
         CircleSectorPainter.draw(
             canvas: canvas,
@@ -36,8 +36,8 @@ class PiePainter {
             center: center,
             radius: radius + 2,
             innerRadius: innerRadius - 2,
-            startAngle: slice.startAngle,
-            endAngle: slice.endAngle,
+            startAngle: slice.startAngle+2,
+            endAngle: slice.endAngle+2,
             fill: slice.fill,
             isBorderRequired: true);
       }
